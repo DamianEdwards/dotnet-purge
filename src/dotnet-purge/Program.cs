cfbd4b2d2a7d364080b9c40d5dd61db218d0d791
Damian Edwards <damian@damianedwards.com>
2025-03-06 10:07:38 -0800

    Works now

--- src/dotnet-purge/Program.cs
+++ src/dotnet-purge/Program.cs
@@ -3,25 +3,82 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
+var targetDir = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+if (!Directory.Exists(targetDir))
+{
+    WriteError($"Directory '{targetDir}' does not exist.");
+    Environment.Exit(1);
+}
+DotnetCli.WorkingDirectory = Path.GetFullPath(targetDir);
+
 // Extract properties
-var properties = await DotnetCli.GetProperties(["Configurations", "BaseIntermediatePath", "BaseOutputPath"]);
+var properties = await DotnetCli.GetProperties(["Configurations", "TargetFrameworks", "BaseIntermediateOutputPath", "BaseOutputPath"]);
+var targetFrameworks = properties["TargetFrameworks"].Split(';');
 var configurations = properties["Configurations"].Split(';');
 
 // Run clean for each configuration
+var isMultiTargeted = targetFrameworks.Length > 1;
 foreach (var configuration in configurations)
 {
-    await DotnetCli.Clean(["--configuration", configuration]);
+    if (isMultiTargeted)
+    {
+        foreach (var framework in targetFrameworks)
+        {
+            Write($"Running 'dotnet clean --configuration {configuration} --framework {framework}'...");
+            await DotnetCli.Clean(["--configuration", configuration, "--framework", framework]);
+            WriteLine(" done!", ConsoleColor.Green);
+        }
+    }
+    else
+    {
+        Write($"Running 'dotnet clean --configuration {configuration}'...");
+        await DotnetCli.Clean(["--configuration", configuration]);
+        WriteLine(" done!", ConsoleColor.Green);
+    }
 }
 
 // Delete intermediate and output directories
-var intermediatePath = properties["BaseIntermediatePath"];
-var outputPath = properties["BaseOutputPath"];
-Directory.Delete(intermediatePath, recursive: true);
-Directory.Delete(outputPath, recursive: true);
+var directortiesToDelete = new[] { properties["BaseIntermediateOutputPath"], properties["BaseOutputPath"] };
+
+foreach (var directory in directortiesToDelete)
+{
+    var path = Path.GetFullPath(directory, DotnetCli.WorkingDirectory);
+    if (Directory.Exists(path))
+    {
+        Write($"Deleting '{path}'...");
+        Directory.Delete(path, recursive: true);
+        WriteLine(" done!", ConsoleColor.Green);
+    }
+}
+
+static void WriteError(string message) => WriteLine(message, ConsoleColor.Red);
+
+static void WriteLine(string message, ConsoleColor? color = default)
+{
+    Write(message, color);
+    Console.WriteLine();
+}
+
+static void Write(string message, ConsoleColor? color = default)
+{
+    if (color is not null)
+    {
+        var originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = color.Value;
+        Console.Write(message);
+        Console.ForegroundColor = originalColor;
+    }
+    else
+    {
+        Console.Write(message);
+    }
+}
 
 static class DotnetCli
 {
     private static readonly string[] CleanArgs = ["clean"];
+
+    public static string? WorkingDirectory { get; set; }
 
     public static Task Clean(string[] args)
     {
@@ -37,16 +94,41 @@ static class DotnetCli
         string[] arguments = ["msbuild", $"-getProperty:{propertiesValue}"];
         var startInfo = GetProcessStartInfo(arguments);
         startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
 
         var process = Start(startInfo);
 
-        var sb = new StringBuilder();
+        var stdout = new StringBuilder();
         process.OutputDataReceived += (sender, e) =>
         {
-            sb.AppendLine(e.Data);
+            if (e.Data is not null)
+            {
+                stdout.AppendLine(e.Data);
+            }
         };
+        var stderr = new StringBuilder();
+        process.ErrorDataReceived += (sender, e) =>
+        {
+            if (e.Data is not null)
+            {
+                stderr.AppendLine(e.Data);
+            }
+        };
+
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
         await process.WaitForExitAsync();
-        var output = JsonSerializer.Deserialize(sb.ToString(), PurgeJsonContext.Default.MsBuildGetPropertyOutput);
+
+        if (process.ExitCode != 0)
+        {
+            Console.Write(stderr.ToString());
+            Console.Write(stdout.ToString());
+            Environment.Exit(process.ExitCode);
+        }
+
+        var stringOutput = stdout.ToString();
+        var output = JsonSerializer.Deserialize(stringOutput, PurgeJsonContext.Default.MsBuildGetPropertyOutput);
         return output?.Properties ?? [];
     }
 
@@ -54,9 +136,9 @@ static class DotnetCli
 
     private static Process Start(ProcessStartInfo startInfo)
     {
-        var process = Process.Start(startInfo);
+        var process = new Process { StartInfo = startInfo };
 
-        return process ?? throw new Exception("Failed to start process");
+        return process.Start() ? process : throw new Exception("Failed to start process");
     }
 
     private static ProcessStartInfo GetProcessStartInfo(IEnumerable<string> arguments)
@@ -67,9 +149,13 @@ static class DotnetCli
             RedirectStandardOutput = false,
             RedirectStandardError = false,
             UseShellExecute = false,
-            CreateNoWindow = true,
-            WindowStyle = ProcessWindowStyle.Hidden
+            CreateNoWindow = true
         };
+
+        if (WorkingDirectory is not null)
+        {
+            info.WorkingDirectory = WorkingDirectory;
+        }
 
         foreach (var arg in arguments)
         {
